5d9ba4e9932037c6e8119ba6082ff2d6156ead75
Theo <theodorejbigelow@gmail.com>
2026-02-25 12:43:56 -0800

    Added UI layouts for future NewGoalPage and DetailPage pages as well as Goal object.

---
+++ Goal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductivityApp
+{
+    public class Goal
+    {
+        public string Name { get; set; }
+        public string Motivation { get; set; }
+        public string TargetDate { get; set; }
+        public double Progress { get; set; }
+    }
+}
